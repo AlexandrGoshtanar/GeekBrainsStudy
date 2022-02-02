d22e039ff2845713a1e663f67cf4429aaeafdccb
AlexandrGoshtanar <Nerfallge@vk.com>
2022-02-02 18:16:53 +0300

    добавили If/Else

---
+++ Example005_LoveUserName/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "вика")
+{
+    Console.WriteLine("Ура! это ВИКА!");
+}
+else 
+{
+    Console.Write("Добро пожаловать, ");
+Console.WriteLine(username);
+}
