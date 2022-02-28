f0f562c821f65d5afe08b2d516f5e90d7e084af3
AlexandrGoshtanar <Nerfallge@vk.com>
2022-02-28 14:46:03 +0300

    lesson7

---
+++ Lesson7_homework/Program.cs
@@ -0,0 +1,52 @@
+﻿void fillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+//Задача 49: Показать двумерный массив размером m×n, заполненный вещественными числами.
+/* int m = 10;
+int n = 5;
+int[,] array = new int[m, n];
+fillArray(array);
+PrintArray(array); */
+//Задача 51: Задать двумерный массив следующим правилом: Aₘₙ = m+n.
+/* int[,] array = new int[5, 5];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = i + j;
+    }
+}
+PrintArray(array); */
+//Задача 53: В двумерном массиве показать позиции числа, заданного пользователем, или указать, что такого элемента нет.
+/* int[,] array = new int[5, 5];
+fillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine("Введите число ");
+int A = int.Parse(Console.ReadLine());
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        if (A == array[i, j]) Console.WriteLine($"Строка:{i + 1},Столбец:{j + 1}");
+    }
+} */
+//Задача 55: Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов.
