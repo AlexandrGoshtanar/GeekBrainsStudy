cc279e38f972b7b2be2adb549e93a3b4578e3972
AlexandrGoshtanar <Nerfallge@vk.com>
2022-07-03 15:37:10 +0300

    commit4

--- ItogovayaRabota/Program.cs
+++ ItogovayaRabota/Program.cs
@@ -1,9 +1,15 @@
-﻿console.Clear;
-string[] ArrString = new string["Sunday","Monday","Tuersday","Wednesday", "Thirsday", "Friday", "Saturday"];
-int numb1 = new Random().Next(0,2);
-int numb2 = new Random().Next(0,6);
-Console.WriteLine(ArrString);
-string[] ArrString2 = new string[];
+﻿string[] ArrString = { "Sunday", "Monday", "Tuersday", "Wednesday", "Thirsday", "Friday", "Saturday" };
+int numb1 = new Random().Next(0, 4);
+for (int i = 0; i < ArrString.Length; i++)
+{
+    Console.Write("{1} ", i, ArrString[i]);
+}
+string[] ArrString2 = new string[numb1];
+Console.WriteLine();
+for (int i = 0; i < ArrString2.Length; i++)
+{
+    int numb2 = new Random().Next(0, 7);
+    Console.Write(ArrString[numb2] + " ");
 
+}
 
-  
