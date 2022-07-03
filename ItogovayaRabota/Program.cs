195f19268e195f7cd68c3054d6dde29276065380
AlexandrGoshtanar <Nerfallge@vk.com>
2022-07-03 15:42:22 +0300

    Final commit

--- ItogovayaRabota/Program.cs
+++ ItogovayaRabota/Program.cs
@@ -1,11 +1,14 @@
-﻿string[] ArrString = { "Sunday", "Monday", "Tuersday", "Wednesday", "Thirsday", "Friday", "Saturday" };
+﻿Console.Clear();
+string[] ArrString = { "Sunday", "Monday", "Tuersday", "Wednesday", "Thirsday", "Friday", "Saturday" };
 int numb1 = new Random().Next(0, 4);
+Console.Write("Старый Массив: ");
 for (int i = 0; i < ArrString.Length; i++)
 {
     Console.Write("{1} ", i, ArrString[i]);
 }
 string[] ArrString2 = new string[numb1];
 Console.WriteLine();
+Console.Write("Новый массив: ");
 for (int i = 0; i < ArrString2.Length; i++)
 {
     int numb2 = new Random().Next(0, 7);
