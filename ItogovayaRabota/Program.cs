1ed47d2c7ea6cd7cafeef6b23a06746c3e7e55e0
AlexandrGoshtanar <Nerfallge@vk.com>
2022-07-03 14:00:15 +0300

    Itogovaya

---
+++ ItogovayaRabota/Program.cs
@@ -0,0 +1,9 @@
+﻿console.Clear;
+string[] ArrString = new string["Sunday","Monday","Tuersday","Wednesday", "Thirsday", "Friday", "Saturday"];
+int numb1 = new Random().Next(0,2);
+int numb2 = new Random().Next(0,6);
+Console.WriteLine(ArrString);
+string[] ArrString2 = new string[];
+
+
+  
